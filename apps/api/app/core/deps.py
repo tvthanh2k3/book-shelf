d6d117970f24002bec68ff0aa895c7e57de614ab
tvthanh2k3 <tvthanh1201@gmail.com>
2026-04-24 16:45:33 +0700

    fix: remove limit upper bound

--- apps/api/app/core/deps.py
+++ apps/api/app/core/deps.py
@@ -2,6 +2,6 @@ from fastapi import Query
 
 
 class PaginationParams:
-    def __init__(self, skip: int = Query(0, ge=0), limit: int = Query(10, ge=1, le=100)):
+    def __init__(self, skip: int = Query(0, ge=0), limit: int = Query(10, ge=1)):
         self.skip = skip
         self.limit = limit
